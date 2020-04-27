e39c435757ebd3a9a4180a9e9f768d98c669d4c9
zahraaalsafwani <zahraaalsafwani@192.168.0.27>
2020-04-26 19:20:25 -0500

    User update

--- src/util/Info.java
+++ src/util/Info.java
@@ -4,5 +4,5 @@ public interface Info {
    public String projectName = "teamproject";
    public String searchWebName = "simpleSearchHB.html";
    public String insertWebName = "simpleInsertHB.html";
-   public String insertUser = "SimpleInsertUserHB.html";
+   public String insertUser = "simpleInsertUser.html";
 }
